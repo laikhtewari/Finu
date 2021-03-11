965547cad53d05bab5d1bd3ad7c5c46728f6d02c
Laikh Tewari <laikhtewari1@gmail.com>
2021-03-11 02:07:44 -0800

    refactored

--- App.js
+++ App.js
@@ -1,53 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image} from 'react-native';
 
-export default function App() {
-	const [value, onChangeText] = React.useState('Useless Placeholder');
+export default function App(props) {
+	const [value, onChangeText] = React.useState('Search Documents');
+	const search_bar_height = 40;
+
 	return (
 		<View style={{}}>
-			<View style={{
-				flex: 1,
-				alignItems: "flex-end",
-				marginTop: 20,
-				marginRight: 20,
-				marginBottom: 30
-			}}>
-				<Image source={require('./assets/mail_icon.png')}/>
-			</View>
-			<View style={{
-				height: 40,
+			<Mail/>
+
+			<Searchbar 
+				search_bar_height={40}
+				default_text='Search Documents'
+			/>
+
+		</View>
+	);
+}
+
+function Mail(props) {
+	return(
+		<View style={{
+			flex: 1,
+			alignItems: "flex-end",
+			marginTop: 20,
+			marginRight: 20,
+			marginBottom: 50
+		}}>
+			<Image source={require('./assets/mail_icon.png')}/>
+			<Text style={{marginTop: 10, color: 'black'}}>Invites</Text>
+		</View>
+	);
+}
+
+function Searchbar(props) {
+	return(
+		<View 
+			style={{
+				height: props.search_bar_height,
 				underlineColor: 'black',
-				borderBottomWidth: 1,
+				borderBottomWidth: 2,
 				marginRight: 20,
 				marginLeft: 20,
 				bottom: 0
+		}}>
+			<TextInput
+				style={{ 
+					height: props.search_bar_height,
+					color: 'black',
+					fontSize: 24
+				}}
+				// onChangeText={}
+				placeholder={props.default_text}
+				placeholderTextColor='grey'
+			/>
+			<View style={{
+				position: 'absolute',
+				right: 0,
+				height: props.search_bar_height,
+				width: props.search_bar_height,
+				padding: 7,
 			}}>
-				<TextInput
-					style={{ 
-						height: 40
-					}}
-					onChangeText={text => onChangeText(text)}
-					value={value}
-				/>
-				<View style={{
-					position: 'absolute',
-					right: 0,
-					height: 40,
-					width: 40,
-				}}>
-					<Image style={{
+				<Image 
+					style={{
 						height: '100%',
 						width: '100%'
 					}}
-					source={require('./assets/search.png')}/>
-				</View>
+					source={require('./assets/search.png')}
+				/>
 			</View>
-			{/*	
-				<View style={{width: 50, height: 50, top: 25, left: 50, backgroundColor: 'powderblue'}} />
-				<View style={{width: 50, height: 50, top: 25, backgroundColor: 'skyblue'}} />
-				<View style={{width: 50, height: 50, top: 25, right: 50, backgroundColor: 'steelblue'}} /> */}
-			</View>
+		</View>
 	);
 }
 
