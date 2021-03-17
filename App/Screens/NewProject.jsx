f4fe387a5a136ac9ea5abca7aaf96f7b9db63664
Laikh Tewari <laikhtewari1@gmail.com>
2021-03-17 13:42:35 -0700

    new project screen dynamic

--- App/Screens/NewProject.jsx
+++ App/Screens/NewProject.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   View,
   Text,
@@ -7,59 +7,119 @@ import {
   Image,
   StyleSheet
 } from "react-native";
+import { useNavigation } from '@react-navigation/native';
 
-export default function App({ navigation }) {
-  return(
-    <View style={{alignItems: 'center', flex: 1}}>
-      <Text style={styles.title}>New Document</Text>
-      <TextInput
-        style={styles.nameInput}
-        // onChangeText={(event.target.text) => updateTextStateFunc()}
-        placeholder={'Document Name'}
-        placeholderTextColor='grey'
-      />
-      <DocType
-        name='Text'
-        description='Best for written agreements, contracts, and leases.'
-        image={require('../../assets/textfile.png')}
-      />
-      <DocType
-        name='Sound'
-        description='Best for spoken agreements, meetings, and quick thoughts.'
-        image={require('../../assets/soundfile.png')}
-      />
-      <DocType
-        name='Visual'
-        description='Best for graphs, photos, and diagrams.'
-        image={require('../../assets/visualfile.png')}
-      />
-      <BottomNavigation navigation={navigation}/>
-    </View>
-  );
+global.FINU_GRAY = '#D9D9D9'
+global.FINU_PURPLE = '#5551FF'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      confirmColor: global.FINU_GRAY,
+      textBorderWidth: 2,
+      soundBorderWidth: 2,
+      visualBorderWidth: 2,
+      selected: null
+    }
+    this.textClicked = this.textClicked.bind(this)
+    this.soundClicked = this.soundClicked.bind(this)
+    this.visualClicked = this.visualClicked.bind(this)
+  }
+
+  textClicked() {
+    this.setState({
+      confirmColor: global.FINU_PURPLE,
+      textBorderWidth: 8,
+      soundBorderWidth: 2,
+      visualBorderWidth: 2,
+      selected: 'Text'
+    })
+  }
+  soundClicked() {
+    this.setState({
+      confirmColor: global.FINU_PURPLE,
+      textBorderWidth: 2,
+      soundBorderWidth: 8,
+      visualBorderWidth: 2,
+      selected: 'Sound'
+    })
+  }
+  visualClicked() {
+    this.setState({
+      confirmColor: global.FINU_PURPLE,
+      textBorderWidth: 2,
+      soundBorderWidth: 2,
+      visualBorderWidth: 8,
+      selected: 'Visual'
+    })
+  }
+
+  render() {
+    return(
+      <View style={{alignItems: 'center', flex: 1}}>
+        <Text style={styles.title}>New Document</Text>
+        <TextInput
+          style={styles.nameInput}
+          // onChangeText={(event.target.text) => updateTextStateFunc()}
+          placeholder={'Document Name'}
+          placeholderTextColor='grey'
+        />
+        <DocType
+          name='Text'
+          description='Best for written agreements, contracts, and leases.'
+          image={require('../../assets/textfile.png')}
+          handleClick={this.textClicked}
+          borderWidth={this.state.textBorderWidth}
+        />
+        <DocType
+          name='Sound'
+          description='Best for spoken agreements, meetings, and quick thoughts.'
+          image={require('../../assets/soundfile.png')}
+          handleClick={this.soundClicked}
+          borderWidth={this.state.soundBorderWidth}
+        />
+        <DocType
+          name='Visual'
+          description='Best for graphs, photos, and diagrams.'
+          image={require('../../assets/visualfile.png')}
+          handleClick={this.visualClicked}
+          borderWidth={this.state.visualBorderWidth}
+        />
+        <BottomNavigation 
+          backgroundColor={this.state.confirmColor}
+        />
+      </View>
+    );
+  }
 }
 
 function DocType(props) {
+  const name = props.name
   return(
-    <View style={styles.docType}>
+    <Pressable 
+      style={[{borderWidth: props.borderWidth}, styles.docType]}
+      onPress={(name) => props.handleClick(name) }
+    >
       <Image
         style={styles.docImage}
         source={props.image}
       />
-
         <View style={styles.docInfo}>
           <Text style={styles.docTitle}>{props.name}</Text>
           <Text>{props.description}</Text>
         </View>
-    </View>
+    </Pressable>
   );
 }
 
-function BottomNavigation({navigation}) {
+function BottomNavigation(props) {
+  const navigation = useNavigation()
   return(
     <View style={styles.bottom}>
       <Pressable
         style={styles.cancel}
-        onPress={() => navigation.goBack()}
+        onPress={navigation.goBack}
       >
         <Image
           source={require("../../assets/backarrow.png")}
@@ -67,8 +127,8 @@ function BottomNavigation({navigation}) {
         <Text style={{color: '#5551FF', marginLeft: 5}}>Cancel</Text>
       </Pressable>
       <Pressable
-        style={styles.confirm}
-        onPress={() => navigation.navigate("Invites")}
+        style={[{backgroundColor: props.backgroundColor}, styles.confirm]}
+        onPress={navigation.goBack}
       >
         <Text style={{color: 'white'}}>Confirm</Text>
       </Pressable>
@@ -105,7 +165,6 @@ const styles = StyleSheet.create({
     marginBottom: 25
   },
   docType: {
-    borderWidth: 2,
     borderRadius: 18,
     borderColor: '#5551FF',
     padding: 10,
@@ -132,7 +191,6 @@ const styles = StyleSheet.create({
   },
   confirm: {
     marginRight: 20,
-    backgroundColor: '#D9D9D9',
     padding: 10,
     borderRadius: 5
   },
@@ -144,3 +202,5 @@ const styles = StyleSheet.create({
     bottom: 35
   }
 });
+
+export default App
